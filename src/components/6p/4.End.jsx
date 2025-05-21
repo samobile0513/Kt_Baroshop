13c0c410bd46abb154b772a69a0bc00e9a097d5a
Im sunsu <fnlunasea@naver.com>
2025-05-21 21:44:31 +0900

    설문조사 완료 페이지 수정

--- src/components/6p/4.End.jsx
+++ src/components/6p/4.End.jsx
@@ -9,7 +9,13 @@ const CharacterSection = () => {
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;
-      setScale(width <= 3840 ? 10 : 1);
+      if (width <= 640) {
+        setScale(1); // 모바일: 확대 없음
+      } else if (width <= 1200) {
+        setScale(1.4); // 태블릿 또는 작은 데스크톱
+      } else {
+        setScale(1); // 큰 데스크톱
+      }
     };
 
     window.addEventListener("resize", handleResize);
@@ -18,28 +24,36 @@ const CharacterSection = () => {
   }, []);
 
   useEffect(() => {
-    if (contentRef.current) {
-      const baseHeight = contentRef.current.offsetHeight;
-      const paddedHeight = baseHeight + 0;
-      setAdjustedHeight(paddedHeight * scale);
-      setLoaded(true);
-    }
+    const img = new Image();
+    img.src = "/End.svg";
+    img.onload = () => {
+      if (contentRef.current) {
+        const baseHeight = contentRef.current.offsetHeight;
+        const paddedHeight = baseHeight + 0;
+        setAdjustedHeight(paddedHeight * scale);
+        setLoaded(true);
+      }
+    };
   }, [scale]);
 
   return (
     <div
-      className="w-full flex justify-center overflow-x-visible pt-[30px]"
+      className="w-full flex justify-center overflow-x-auto pt-[30px]"
       style={{ minHeight: adjustedHeight }}
     >
       <div
         ref={contentRef}
-        className="w-[820px] flex flex-col items-center"
+        className="w-[820px] max-w-full flex flex-col items-center"
         style={{
-          zoom: scale,
+          transform: `scale(${scale})`,
           transformOrigin: "top center",
         }}
       >
-        <img src="/End.svg" alt="5_1" />
+        <img
+          src="/End.svg"
+          alt="5_1"
+          style={{ objectFit: "contain", width: "100%", height: "auto" }}
+        />
       </div>
     </div>
   );
